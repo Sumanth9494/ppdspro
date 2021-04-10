5d4589c336a56fa343f434f4ad93b5b9a22b4598
Sumanth Reddy <47381092+Sumanth9494@users.noreply.github.com>
2021-04-10 16:46:50 +0530

    dsgfsdgwergw4gw

--- PPDS_Project/settings.py
+++ PPDS_Project/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_URL = 'https://github.com/Sumanth9494/ppdspro/tree/master/static/'
+STATICFILES_DIRS=[https://github.com/Sumanth9494/ppdspro/tree/master/static]
 
 SESSION_COOKIE_AGE=400
